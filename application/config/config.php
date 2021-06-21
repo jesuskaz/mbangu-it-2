4a2f3b35a05ff44aa47c7c0016c2c3b88f25bd3b
Authentic <patriczum@gmail.com>
2021-06-21 18:34:30 +0200

    base url ok

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://mbangu-it.conm/';
+$config['base_url'] = 'http://www.mbangu-it.com/';
 
 /*
 |--------------------------------------------------------------------------
